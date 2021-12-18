b25327d9affc1d3a0944761ce589e78b78813a77
ramakrushnarao <rakiaprdc@gmail.com>
2021-12-18 22:25:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./Components/About/About";
+import Blog from "./Components/Blog/Blog";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home";
+import Project from "./Components/Project/Project";
+import Service from "./Components/Services/Services";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home></Home>
+      <About></About>
+      <Service></Service>
+      <Project></Project>
+      <Blog></Blog>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
